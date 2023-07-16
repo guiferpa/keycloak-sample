d5c57ae6830499ab51a1f9fc2174940e5c8082b6
Guilherme Paixão <guiferpa@gmail.com>
2023-07-16 14:34:59 -0300

    fix: fixed frontend spa logout

--- frontend/spa/src/components/PrivateRoute.tsx
+++ frontend/spa/src/components/PrivateRoute.tsx
@@ -1,7 +1,7 @@
-import React from 'react';
-import { Outlet, RouteProps } from 'react-router-dom';
-import axios, { AxiosResponse, AxiosInstance } from 'axios';
-import HttpStatusCodes from 'http-status-codes';
+import React from "react";
+import { Outlet, RouteProps } from "react-router-dom";
+import axios, { AxiosResponse, AxiosInstance } from "axios";
+import HttpStatusCodes from "http-status-codes";
 
 interface PrivateRouteProps extends RouteProps {}
 
@@ -11,29 +11,34 @@ const isUserAuthenticated = async (): Promise<boolean> => {
     const realm = process.env["REACT_APP_KEYCLOAK_REALM"];
     const requester: AxiosInstance = axios.create({
       baseURL: host,
-      timeout: 5000 // 5 seconds
+      timeout: 5000, // 5 seconds
     });
-    const resp: AxiosResponse<any> = await requester.get(`/realms/${realm}/protocol/openid-connect/userinfo`, {
-      headers: {
-        'Authorization': `Bearer ${localStorage.getItem(`${process.env["REACT_APP_KEYCLOAK_CLIENT_ID"]}:access_token`)}`
+    const resp: AxiosResponse<any> = await requester.get(
+      `/realms/${realm}/protocol/openid-connect/userinfo`,
+      {
+        headers: {
+          Authorization: `Bearer ${localStorage.getItem(
+            `${process.env["REACT_APP_KEYCLOAK_CLIENT_ID"]}:access_token`
+          )}`,
+        },
       }
-    });
+    );
 
     if (resp.status === HttpStatusCodes.UNAUTHORIZED) return false;
 
     return true;
-  } catch(err) {
+  } catch (err) {
     return false;
   }
-}
+};
 
-const PrivateRoute: React.FunctionComponent<PrivateRouteProps> = (props) => {
+const PrivateRoute: React.FunctionComponent<PrivateRouteProps> = () => {
   const [isAuthenticated, setIsAuthenticated] = React.useState(false);
 
   React.useEffect(() => {
     isUserAuthenticated()
       .then((result) => setIsAuthenticated(result))
-      .catch(() => setIsAuthenticated(false))
+      .catch(() => setIsAuthenticated(false));
   }, [isAuthenticated]);
 
   if (isAuthenticated) {
@@ -41,6 +46,6 @@ const PrivateRoute: React.FunctionComponent<PrivateRouteProps> = (props) => {
   }
 
   return <span>Page not found</span>;
-}
+};
 
 export default PrivateRoute;
--- frontend/spa/src/components/Profile.tsx
+++ frontend/spa/src/components/Profile.tsx
@@ -1,19 +1,21 @@
-import React from 'react';
-import qs from 'query-string';
-import axios, { AxiosInstance, AxiosResponse } from 'axios';
-import HttpStatusCodes from 'http-status-codes';
+import React from "react";
+import qs from "query-string";
+import axios, { AxiosInstance, AxiosResponse } from "axios";
+import HttpStatusCodes from "http-status-codes";
 
 const getLogOutURL = (): string => {
   const host = process.env["REACT_APP_KEYCLOAK_HOST"];
   const realm = process.env["REACT_APP_KEYCLOAK_REALM"];
   const endpoint = `/realms/${realm}/protocol/openid-connect/logout`;
-  const params = { 
-    "post_logout_redirect_uri": `${window.location.origin}/login`,
-    "id_token_hint": localStorage.getItem(`${process.env["REACT_APP_KEYCLOAK_CLIENT_ID"]}:id_token`)
+  const params = {
+    // "post_logout_redirect_uri": `${window.location.origin}/login`,
+    id_token_hint: localStorage.getItem(
+      `${process.env["REACT_APP_KEYCLOAK_CLIENT_ID"]}:id_token`
+    ),
   };
 
   return `${host}${endpoint}?${qs.stringify(params)}`;
-}
+};
 
 interface User {
   name: string;
@@ -25,21 +27,26 @@ const getUser = async (): Promise<User | null> => {
     const realm = process.env["REACT_APP_KEYCLOAK_REALM"];
     const requester: AxiosInstance = axios.create({
       baseURL: host,
-      timeout: 5000 // 5 seconds
+      timeout: 5000, // 5 seconds
     });
-    const resp: AxiosResponse<any> = await requester.get(`/realms/${realm}/protocol/openid-connect/userinfo`, {
-      headers: {
-        'Authorization': `Bearer ${localStorage.getItem(`${process.env["REACT_APP_KEYCLOAK_CLIENT_ID"]}:access_token`)}`
+    const resp: AxiosResponse<any> = await requester.get(
+      `/realms/${realm}/protocol/openid-connect/userinfo`,
+      {
+        headers: {
+          Authorization: `Bearer ${localStorage.getItem(
+            `${process.env["REACT_APP_KEYCLOAK_CLIENT_ID"]}:access_token`
+          )}`,
+        },
       }
-    });
+    );
 
     if (resp.status === HttpStatusCodes.UNAUTHORIZED) return null;
 
-    return resp.data
-  } catch(err) {
+    return resp.data;
+  } catch (err) {
     return null;
   }
-}
+};
 
 const Profile: React.FunctionComponent = () => {
   const [user, setUser] = React.useState<User | null>(null);
@@ -47,7 +54,7 @@ const Profile: React.FunctionComponent = () => {
   React.useEffect(() => {
     getUser()
       .then((result) => setUser(result))
-      .catch(() => setUser(null))
+      .catch(() => setUser(null));
   }, []);
 
   return (
@@ -61,7 +68,7 @@ const Profile: React.FunctionComponent = () => {
       )}
       <a href={getLogOutURL()}>Log out</a>
     </div>
-  )
-}
+  );
+};
 
 export default Profile;
